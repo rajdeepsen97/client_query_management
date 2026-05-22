7712c413ab7718ddc6d1aa3619ca940016981a45
Rajdeep Sen <angaraj97@gmail.com>
2026-05-22 23:34:19 +0530

    chore: remove test_connection file

--- test_connection.py
+++
@@ -1,8 +0,0 @@
-from db import create_connection
-
-connection = create_connection()
-
-if connection.is_connected():
-    print("MySQL Connected Successfully!")
-else:
-    print("Connection Failed")
